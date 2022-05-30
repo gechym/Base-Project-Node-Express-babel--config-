92db574deb05f01b6baf7853989378975c86730f
ndbao <nguyenducbao166@gmail.com>
2022-05-30 12:26:38 +0700

    base route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,59 @@
-require('dotenv').config({path : './config.env'})
+require('dotenv').config({ path: './config.env' });
 import express from 'express';
-  
 const app = express();
-  
-app.get('/',(req,res) => {
-    res.send('Hello word');
-})
-  
+
+//config
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.static(`${__dirname}/public`)); // khai các file
+
+const getTours = (req, res) => {
+    res.status(200).json({
+        message: 'success',
+        data: [
+            {
+                id: 0,
+                name: 'The Forest Hiker',
+            },
+        ],
+    });
+};
+const getTour = (req, res) => {
+    console.log(req.body);
+    res.status(200).json({ ...req.body, id: req.params.id });
+};
+
+const createTour = (req, res) => {
+    res.status(200).json({
+        message: 'success',
+        data: {
+            tours: req.body,
+        },
+    });
+};
+
+const updateTour = (req, res) => {
+    const { id } = req.params;
+
+    res.status(200).json({
+        message: 'update success',
+        data: {
+            id,
+            ...req.body,
+        },
+    });
+};
+
+app.route('/api/v1/tours').get(getTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).put(updateTour);
+
 const PORT = process.env.PORT || 5000;
-  
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}`);
-})
+});
+
+export default app;
