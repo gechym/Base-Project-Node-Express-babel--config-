7e0b3d2be27774135a2ebbcecf0cf1d42803e06f
ndbao <nguyenducbao166@gmail.com>
2022-06-01 12:14:34 +0700

    json web token

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 import { User } from '../module';
 import AppError from '../util/AppError';
 import catchAsync from '../util/catchAsync';
@@ -8,15 +10,29 @@ export const signUp = catchAsync(async (req, res, next) => {
         return next(new AppError('Vui lòng cung cấp đầy đủ thông tin nha', 404));
     }
 
-    const newUser = await sUser.create({
+    const newUser = await User.create({
         name,
         email,
         password,
         passwordConfig,
     });
 
+    const token = jwt.sign(
+        {
+            id: newUser.id,
+            name: newUser.name,
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: process.env.TOKEN_JWT_HET_HAN,
+        },
+    );
+
+    console.log(token);
+
     res.status(200).json({
         message: 'success',
+        token,
         data: {
             user: newUser,
         },
